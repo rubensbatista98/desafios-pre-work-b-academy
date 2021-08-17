8b0951423e6d4b620e85c20dc481bbc554659ef2
Rubens Batista <batistarubinho@gmail.com>
2021-08-17 13:36:36 -0300

    feat: resolve fourth exercise of day one. Add a link to hide/show app when is clicked

--- day-one/src/main.js
+++ day-one/src/main.js
@@ -1,6 +1,26 @@
 import "./style.css";
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const $link = document.querySelector('[data-js="link"]');
+const $app = document.querySelector('[data-js="app"]');
+
+$app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `;
+
+$link.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const HIDE_CLASSNAME = "-hide";
+  const shouldHide = $app.classList.contains(HIDE_CLASSNAME);
+
+  if (shouldHide) {
+    $app.classList.remove(HIDE_CLASSNAME);
+    $link.textContent = "Esconder boas-vindas";
+
+    return;
+  }
+
+  $app.classList.add(HIDE_CLASSNAME);
+  $link.textContent = "Mostrar boas-vindas";
+});
